f0d68c973dd49e0c9431cb0d14086e4628bbb039
yasudamasaharu <mshr-0926@ur.ttn.ne.jp>
2022-05-16 17:40:25 +0900

    vimに嫌気がさした為一時中断

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)  
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+  const reset = () => setCount(previousCount = 0)
+ 
   return (
-    <div>
-	This is a template for React App
-    </div>
+    <>
+      <div>count: {count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <br/>
+      <button onClick={increment2}>+1</button>
+      <button onClick={decrement2}>-1</button>
+      <br/>
+      <button onClick={reset}>Reset</button>
+    </>
   );
 }
 
