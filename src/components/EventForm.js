22a23d4f383cfd8b776bbb483aeee9b6864bb6ca
yasudamasaharu <mshr-0926@ur.ttn.ne.jp>
2022-05-20 16:21:05 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,7 +1,13 @@
 import React, { useContext, useState } from 'react';
 
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS,
+} from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils.js';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -17,6 +23,12 @@ const EventForm = () => {
       body,
     });
 
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601(),
+    });
+
     setTitle('');
     setBody('');
   };
@@ -27,7 +39,14 @@ const EventForm = () => {
       '全てのイベントを本当に削除しても良いですか？'
     );
 
-    if (result) dispatch({ type: DELETE_ALL_EVENTS });
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601,
+      });
+    }
   };
 
   const unCreatable = title === '' || body === '';
